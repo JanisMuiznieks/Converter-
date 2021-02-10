fba5e2307f950d0dab7636bffbd5c2d7dcc2aac6
JanisMuiznieks <janis.muiznieks8@gmail.com>
2021-02-10 16:03:11 +0200

    adding calculation from Temp C to F, K

--- Converter+/Controller/DayFinderViewController.swift
+++ Converter+/Controller/DayFinderViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  DayFinderViewController.swift
 //  Converter+
 //
 //  Created by janis.muiznieks on 10/02/2021.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class DayFinderViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ Converter+/Controller/TempViewController.swift
@@ -0,0 +1,59 @@
+//
+//  TempViewController.swift
+//  Converter+
+//
+//  Created by janis.muiznieks on 10/02/2021.
+//
+
+import UIKit
+
+class TempViewController: UIViewController {
+
+   
+    @IBOutlet weak var celsiusLabel: UILabel!
+    @IBOutlet weak var tempSlider: UISlider!
+    @IBOutlet weak var convertedTempLabel: UILabel!
+    @IBOutlet weak var tempSegmentControl: UISegmentedControl!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func tempSliderChanged(_ sender: UISlider, forEvent event: UIEvent) {
+        updateTempSliderLabel(value: sender.value)
+    }
+    
+    @IBAction func tempSegmentControlChanged(_ sender: UISegmentedControl) {
+        
+        
+        
+    }
+    
+    func convertTempFrom(celsius: Int) -> (fahrenheit: Double, kelvin: Double){
+        let fahrenheit = (Double(celsius) * 9 / 5 ) + 32
+        let kelvin = Double(celsius) + 273.15
+        
+        return(fahrenheit, kelvin)
+    }
+    
+    func updateTempSliderLabel(value: Float){
+        let celsiumTemp = Int(value)
+        celsiusLabel.text = "\(celsiumTemp) º C"
+        
+        var tempString = ""
+        switch tempSegmentControl.selectedSegmentIndex {
+        case 0:
+            let fahreinheitTempString = String(format: "%.2F", convertTempFrom(celsius: celsiumTemp).fahrenheit)
+            tempString = fahreinheitTempString + " ºF"
+        default:
+            let kelvinTempString =  String(format: "%.2F", convertTempFrom(celsius: celsiumTemp).kelvin)
+            tempString = kelvinTempString + " ºK"
+        }
+        
+        convertedTempLabel.text = tempString
+    }
+ //MARK: - Navigation
+    
+    
+}
